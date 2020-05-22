4c34c284a25500097b6f11e1dd32422b6606c106
winson0228 <winson0228@gmail.com>
2020-05-21 19:54:46 -0700

    added express as dependecy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require("path");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
