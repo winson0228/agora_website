cc32a4d85af874b9790dc842e8193bc6aa5521f4
winson0228 <winson0228@gmail.com>
2020-05-21 19:42:08 -0700

    restucture project and now hostable with nodejs

---
+++ app.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    "Web server at port " + (process.env.PORT || 3000) + " is running..."
+  );
+});
+
+app.use(express.static(path.join(__dirname, "public")));
